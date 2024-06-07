67abd28fd4a330c8150e3ec082ef3feb37208114
Vikashuvi <vikashuvi07@gmail.com>
2024-06-07 23:00:52 +0530

    Faq is created

--- script.js
+++ script.js
@@ -82,8 +82,6 @@
         });
     });
 
-     // Show or hide slide buttons based on scroll position
-   
 
     // Update scrollbar thumb position based on image scroll
     const updateScrollThumbPosition = () => {
@@ -105,3 +103,29 @@
 
 window.addEventListener("resize", initSlider);
 window.addEventListener("load", initSlider);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach(item => {
+    const questionButton = item.querySelector(".faq-question");
+    const answer = item.querySelector(".faq-answer");
+    const icon = questionButton.querySelector(".faq-icon");
+
+    questionButton.addEventListener("click", () => {
+      const isVisible = answer.classList.contains("show");
+      
+      // Close all other answers
+      faqItems.forEach(i => {
+        i.querySelector(".faq-answer").classList.remove("show");
+        i.querySelector(".faq-icon").classList.remove("open");
+      });
+
+      if (!isVisible) {
+        answer.classList.add("show");
+        icon.classList.add("open");
+      }
+    });
+  });
+});
